b80c89cb56dffc8417f343cfe8ec68613994415a
law1024 <286519949@qq.com>
2015-04-16 00:08:06 +0800

    add another function

--- javascript/longest_palindromic_substring.js
+++ javascript/longest_palindromic_substring.js
@@ -52,4 +52,41 @@ var longestPalindrome = function(s) {
     return str;
 };
 
+// version 2 将两个循环合并成了一个
+
+var longestPalindrome = function(s) {
+    //首先对特殊情况进行判断
+    if (!s) {
+        return 0;
+    }
+    if (s.length === 1) {
+        return s;
+    }
+    // 如何合并两个循环
+    // 分析，如果一下子看不出来，就一步步写下来，然后归纳总结
+    var left, right;
+    var i = 0,
+        l = s.length,
+        n = 2 * l - 1;
+    var str = '';
+
+    for (; i < n; i ++) {
+        //right不变，left和奇偶有关
+        right = (left = 0|i / 2) + 1;
+        if (i % 2 === 0) {
+            left--;
+        }
+        for (; left >= 0 && right < l; left--, right ++) {
+            if (s[left] !== s[right]) {
+                break;
+            }
+            if (str.length < right - left + 1) {
+                str = s.slice(left, right + 1);
+            }
+        }
+    }
+    return str;
+}
+
+console.log(longestPalindrome('ccc'));
 // 第二种解法采用动态规划，http://www.tuicool.com/articles/eQNfqey
