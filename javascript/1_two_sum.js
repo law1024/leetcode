18feb8b5c548d8dcf1fd2bd6ff155a03ee80b717
law1024 <286519949@qq.com>
2015-07-05 13:21:09 +0800

    modify space

--- javascript/1_two_sum.js
+++ javascript/1_two_sum.js
@@ -10,15 +10,15 @@
  */
 
 var twoSum = function(nums, target) {
-	//对每一项计算差值，并寻找该差值是否存在
+    //对每一项计算差值，并寻找该差值是否存在
     for (var i = 0, num; (num = nums[i]) !== undefined; i ++) {
 
-    	var dis = target - num;
-    	var n = nums.lastIndexOf(dis);
+        var dis = target - num;
+        var n = nums.lastIndexOf(dis);
 
-    	if (~n && n > i) {
-    		return [i + 1, n + 1];
-    	}
+        if (~n && n > i) {
+            return [i + 1, n + 1];
+        }
     }
 };
 
@@ -31,31 +31,31 @@ var twoSum = function(nums, target) {
  */
 
 var twoSum = function(nums, target) {
-	//先对数组进行排序
-	var temp = nums.slice();
-	//sort函数会改变数组本身
-	nums.sort(function(a, b) {
-		return a - b;
-	});
+    //先对数组进行排序
+    var temp = nums.slice();
+    //sort函数会改变数组本身
+    nums.sort(function(a, b) {
+        return a - b;
+    });
 
-	var i = 0,
-		l = nums.length - 1;
+    var i = 0,
+        l = nums.length - 1;
 
-	//从两边进行查找
-	while (i !== l) {
-		var sum = nums[i] + nums[l];
-		if (sum < target) {
-			i ++;
-		} else if (sum > target) {
-			l --;
-		} else {
-			//find it!	
-			var a = temp.indexOf(nums[i]);
-			var b = temp.lastIndexOf(nums[l]);
+    //从两边进行查找
+    while (i !== l) {
+        var sum = nums[i] + nums[l];
+        if (sum < target) {
+            i ++;
+        } else if (sum > target) {
+            l --;
+        } else {
+            //find it!	
+            var a = temp.indexOf(nums[i]);
+            var b = temp.lastIndexOf(nums[l]);
 
-			return [a+1, b+1].sort(function(a, b) {
-				return a - b;
-			});
-		}
-	}
+            return [a+1, b+1].sort(function(a, b) {
+                return a - b;
+            });
+        }
+    }
 }
